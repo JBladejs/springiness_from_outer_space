e0f633afa917a997b565104249068df24b120ba2
Bladejs <JBladejs@gmail.com>
2021-01-31 02:13:04 +0100

    Another monster refactor

--- core/src/com/physics/project/entities/monster/Monster.kt
+++ core/src/com/physics/project/entities/monster/Monster.kt
@@ -1,12 +1,12 @@
 package com.physics.project.entities.monster
 
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.math.MathUtils.*
 import com.badlogic.gdx.utils.Array
 import com.physics.project.Color
 import com.physics.project.entities.Entity
 import com.physics.project.util.render
 import kotlin.math.cos
-import kotlin.math.sin
 import kotlin.math.sqrt
 
 class Monster(var x: Float, var y: Float, startingSize: Float, tentacleAmount: Int, tentacleLength: Int ) : Entity {
@@ -14,7 +14,7 @@ class Monster(var x: Float, var y: Float, startingSize: Float, tentacleAmount: I
     private val centralPart = MonsterPart(x, y, startingSize, Color(240, 10, 10))
     private val parts = Array<MonsterPart>()
     private val springs = Array<Spring>()
-    private val v = 5f
+//    private val v = 5f //not used yet
 
     private val minSize = 10f
 
@@ -23,20 +23,23 @@ class Monster(var x: Float, var y: Float, startingSize: Float, tentacleAmount: I
         for( i in 0 until tentacleLength) {
             for( j in 1..tentacleAmount) {
                 val currentLengthSize = startingSize * ((tentacleLength-i-1).toFloat()/tentacleLength) + (minSize*((i+1f)/tentacleLength))
-                parts.add(MonsterPart(x + 110f * (i+1) * sin(2f * Math.PI / tentacleAmount * j).toFloat(), y + 110f * (i+1) * cos(2f * Math.PI / tentacleAmount * j).toFloat(),currentLengthSize))
-                if(i>0)
-                    springs.add(Spring(parts[j+((i-1)*tentacleAmount)], parts[j+(i*tentacleAmount)]))
-                else
-                    springs.add(Spring(parts[0], parts[j+(i*tentacleAmount)]))
+                createPart(110f * (i+1) * sin(2f * PI / tentacleAmount * j), 110f * (i+1) * cos(2f * PI / tentacleAmount * j), currentLengthSize)
+                if (i > 0) connect(parts[j+((i-1)*tentacleAmount)], parts[j+(i*tentacleAmount)])
+                else connect(parts[0], parts[j+(i*tentacleAmount)])
             }
         }
     }
+
+    private fun createPart(x: Float, y: Float, size: Float) = parts.add(MonsterPart(this.x + x, this.y + y, size))
+
+    private fun connect(part1: MonsterPart, part2: MonsterPart) = springs.add(Spring(part1, part2))
 
     override fun update() {
         parts.forEach {
             it.update()
         }
         //Collision check TODO: verify if it's good enough
+        //TODO: implement proper collision system
         for (i in 0 until parts.size - 1) {
             for (j in i + 1 until parts.size) {
                 if(parts[j].radius + parts[i].radius > sqrt((parts[j].x - parts[i].x)*(parts[j].x - parts[i].x)+((parts[j].y - parts[i].y) * (parts[j].y - parts[i].y)))){
