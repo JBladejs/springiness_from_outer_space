75cc9dfee40e1bf35766e0d78fb6827c4d9a20df
Bladejs <JBladejs@gmail.com>
2021-01-31 21:03:19 +0100

    Added actual sprite object to the Player.

--- core/src/com/physics/project/entities/player/Player.kt
+++ core/src/com/physics/project/entities/player/Player.kt
@@ -2,6 +2,7 @@ package com.physics.project.entities.player
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.physics.project.graphics.Color
 import com.physics.project.entities.Entity
@@ -10,15 +11,18 @@ import com.physics.project.util.setColor
 
 class Player(var x: Float, var y: Float) : Entity {
     //TODO: investigate libGDX internal Asset Manager
-    private val sprite = Texture(Gdx.files.internal("spaceship.png"))
+    private val sprite = Sprite(Texture("spaceship.png"))
 
     override fun update(delta: Float) {}
 
     override fun render(renderer: Renderer) {
-        renderer.sprites.draw(sprite, x, y)
+        sprite.rotation = 30f
+        sprite.x = x
+        sprite.y = y
+        sprite.draw(renderer.sprites)
     }
 
     override fun dispose() {
-        sprite.dispose()
+        sprite.texture.dispose()
     }
 }
