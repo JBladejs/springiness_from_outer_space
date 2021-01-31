6fcf94ba60277674047ec4edc98004eb5c68ac8c
Bladejs <JBladejs@gmail.com>
2021-01-31 21:25:23 +0100

    Figured out a proper way to rotate a sprite.

--- core/src/com/physics/project/entities/player/Player.kt
+++ core/src/com/physics/project/entities/player/Player.kt
@@ -13,13 +13,20 @@ class Player(var x: Float, var y: Float) : Entity {
     //TODO: investigate libGDX internal Asset Manager
     private val sprite = Sprite(Texture("spaceship.png"))
 
+    init {
+        sprite.setSize(100f, 100f)
+        sprite.x = x - sprite.width / 2f
+        sprite.y = y - sprite.height / 2f
+        sprite.setOrigin(sprite.width / 2f, sprite.height / 2f)
+        sprite.rotation = 90f
+    }
+
     override fun update(delta: Float) {}
 
     override fun render(renderer: Renderer) {
-        sprite.rotation = 30f
-        sprite.x = x
-        sprite.y = y
         sprite.draw(renderer.sprites)
+        renderer.shapes.set(ShapeRenderer.ShapeType.Filled)
+        renderer.shapes.circle(x, y, 20f)
     }
 
     override fun dispose() {
