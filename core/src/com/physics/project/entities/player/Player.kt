a9e4534e3514cc4cf4c48245a789ed26ca7e9497
Bladejs <JBladejs@gmail.com>
2021-01-31 21:35:21 +0100

    Optimized sprite drawing.

--- core/src/com/physics/project/entities/player/Player.kt
+++ core/src/com/physics/project/entities/player/Player.kt
@@ -12,16 +12,22 @@ import com.physics.project.util.setColor
 class Player(var x: Float, var y: Float) : Entity {
     //TODO: investigate libGDX internal Asset Manager
     private val sprite = Sprite(Texture("spaceship.png"))
+    private val centerX: Float
+    private val centerY: Float
+    private var rotation = 0f
 
     init {
         sprite.setSize(100f, 100f)
-        sprite.x = x - sprite.width / 2f
-        sprite.y = y - sprite.height / 2f
-        sprite.setOrigin(sprite.width / 2f, sprite.height / 2f)
-        sprite.rotation = 90f
+        centerX = sprite.width * 0.5f
+        centerY = sprite.height * 0.5f
+        sprite.setOrigin(centerX, centerY)
     }
 
-    override fun update(delta: Float) {}
+    override fun update(delta: Float) {
+        sprite.x = x - centerX
+        sprite.y = y - centerY
+        sprite.rotation = rotation
+    }
 
     override fun render(renderer: Renderer) {
         sprite.draw(renderer.sprites)
