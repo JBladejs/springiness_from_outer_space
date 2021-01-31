02e2d007cb3f62c3ab3e5e25a1b386cf2227f779
Rawaw <rafal.wawrzyniak@wir.pl>
2021-01-31 23:11:06 +0100

    Reworked collision system, working

--- core/src/com/physics/project/collisions/CircleCollider.kt
+++ core/src/com/physics/project/collisions/CircleCollider.kt
@@ -1,5 +1,6 @@
 package com.physics.project.collisions
 
+import com.badlogic.gdx.math.MathUtils
 import kotlin.math.pow
 import kotlin.math.sqrt
 
@@ -16,7 +17,16 @@ class CircleCollider(var x: Float, var y: Float, val radius: Float, val tag: Col
     fun update(x: Float, y: Float) {
         this.x = x
         this.y = y
+
     }
 
     fun collides(collider: CircleCollider): Boolean = sqrt((collider.x - x).pow(2) + (collider.y - y).pow(2)) < radius + collider.radius
+    fun collides(collider: SquareCollider): Boolean {
+        var angleToCirc = MathUtils.atan2(yHit - y, xHit - x)
+        var angleDiffrences = (angleToCirc - (collider.rotation*(3.14f/180f)))%6.28f
+        var percent = MathUtils.sin(2f * angleDiffrences)
+        var squareRadius = collider.size*0.5f+(collider.diagonal*percent)
+
+        return sqrt((collider.x - x).pow(2) + (collider.y - y).pow(2)) <  squareRadius + radius
+    }
 }
---
+++ core/src/com/physics/project/collisions/SquareCollider.kt
@@ -0,0 +1,33 @@
+package com.physics.project.collisions
+
+import com.badlogic.gdx.math.MathUtils.*
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class SquareCollider(var x: Float, var y: Float, var size: Float,var rotation: Float, val tag: CollisionTag) {
+    var isColliding = false
+    var xHit = 500f
+    var yHit = 600f
+    var tagHit = CollisionTag.EMPTY
+    val diagonal = sqrt(2*size.pow(2))- (size*0.5f)
+
+    init {
+        this.size = size*0.5f
+        this.rotation = rotation*(3.14f/180f)
+        //CollisionSystem.add(this)
+    }
+
+    fun update(x: Float, y: Float) {
+        this.x = x
+        this.y = y
+    }
+
+    fun collides(collider: CircleCollider): Boolean {
+        var angleToCirc = atan2(yHit - y,xHit - x)
+        var angleDiffrences = (angleToCirc - rotation)%6.28f
+        var percent = sin(2f*angleDiffrences)
+        var squareRadius = size*0.5f+(diagonal*percent)
+
+        return sqrt((collider.x - x).pow(2) + (collider.y - y).pow(2)) <  squareRadius + collider.radius
+    }
+}
