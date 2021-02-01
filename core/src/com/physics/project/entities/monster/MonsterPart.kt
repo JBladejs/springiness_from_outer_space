fc5a7aa66546a91a818e60ad304e91e8beed0c6f
Bladejs <JBladejs@gmail.com>
2021-02-01 12:10:36 +0100

    Collision fix

--- core/src/com/physics/project/entities/monster/MonsterPart.kt
+++ core/src/com/physics/project/entities/monster/MonsterPart.kt
@@ -17,7 +17,7 @@ internal data class MonsterPart(var monster: Monster, var x: Float, var y: Float
     companion object {
         private const val k = 0.001f
         private const val pushForce = 0.01f
-        private const val hitForce = 500f
+        private const val hitForce = 5f
         private const val speed = 10f
     }
 
@@ -66,7 +66,7 @@ internal data class MonsterPart(var monster: Monster, var x: Float, var y: Float
     fun move(toX: Float, toY: Float) = move(toX, toY, true)
 
     private fun hit(hitX: Float, hitY: Float) {
-        val direction = atan2((x - this.x), (y - this.y))
+        val direction = atan2((hitX - this.x), (hitY - this.y))
         vx -= hitForce * sin(direction)
         vy -= hitForce * cos(direction)
     }
